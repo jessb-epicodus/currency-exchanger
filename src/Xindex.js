57d1309c221d630387f1d7627f80baf8e2689d7f
jb <jessb.epicodus@gmail.com>
2022-02-11 15:17:20 -0800

    starting over

---
+++ src/Xindex.js
@@ -0,0 +1,46 @@
+// A user should be able to enter an amount (in U.S. dollars) and then specify another currency (such as the South Korean won). The user should then see the total amount they entered in converted currency. In the example above, a user might enter 10 dollars and then see that amount in South Korean won.
+// Users should be able to convert U.S. currency into at least 5 other types of currency.
+// If the API call results in an error (any message not a 200 OK), the application should return a notification to the user that states what the error is. (That means the error should show up in the DOM, not in the console.)
+// If the query response doesn't include that particular currency, the application should return a notification that states the currency in question doesn't exist. (Note: Even if you use a dropdown menu to specify currencies instead of a form field, you'll still need to add this functionality to your code.)
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyExchange from './js/Xcurrency-exchange';
+
+function clearFields() {
+  $('#inputUSD').val("");
+  $("input:radio[name=newCurrency]").attr('checked',false);
+  $('.showResult').text("");
+  $('.showError').text("");
+}
+
+$(document).ready(function() {
+  $("form#currency-exchanger").submit(function(event) {
+    event.preventDefault();
+    clearFields();
+    // let USD = parseInt($('#inputUSD').val());
+
+    // if (!USD)
+    //   return $(".showResult").text("Enter a positive amount");
+
+    let currency = $("input:radio[name=newCurrency]:checked").val();
+    let promise = CurrencyExchange.getCurrency(currency);
+    promise.then(function(response) {
+      const results = JSON.parse(response);
+      let USD = parseInt($('#inputUSD').val());
+      $('.showResult').text(`${Math.round(results.conversion_rate * USD)} ${results.target_code}`);
+      $('.showError').text("");
+    }, function(error) {
+      $('.showError').text(`There was an error: ${error}`);
+    });
+  });
+});
+
+// if (error === "unsupported-code") {
+//   $('.showError').text("Choose a currency from the provided list");
+// } else if (error === "invalid-key" ||  error === "inactive-account") {
+//   $('.showError').text("Follow instructions for obtaining a valid API key");
+// } else if (error === "quota-reached" ||  error === "quota-reached") {
+//   $('.showError').text("See documentation for directions & limitations using this API");
+// } else {}
