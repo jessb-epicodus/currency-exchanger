d72d748147c78e06f6d90f03ef22822199a1685f
jb <jessb.epicodus@gmail.com>
2022-02-11 10:11:44 -0800

    establish general layout for html w/ bs styling; WIP: js & calling API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '.././css/styles.css';
+import './css/styles.css';
+import CurrencyService from './js/currency-service.js';
+
+function clearFields() {
+  $('#input-USD').val("");
+  $('.showResults').text("");
+}
+
+$(document).ready(function() {
+  $('form#currency-exchanger').submit(function() {
+    event.preventDefault();
+    const USD = $('#input-USD').val();
+    $('#input-USD').val("");
+    clearFields();
+    let promise = CurrencyService.getCurrency(USD);
+    promise.then(function(response) {
+      const exchange= JSON.parse(response);
+      $('.showResult').text(`${exchange}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
